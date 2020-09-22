85a18b73d71e46b1e01b329ce41919e7e4232823
Daniel Zarate <danielzarate@MacBook-Pro-de-Daniel.local>
2020-09-22 13:00:02 -0500

    Hola

---
+++ Clase10/programacionfuncional.py
@@ -0,0 +1,12 @@
+ejemplo = lambda
+def line_desing():
+    print('#'*60)
+line_desing()
+line = lambda : print('#'*60)
+print("Ahora usando lambda")
+line ()
+
+print("caso2")
+def line_desing_e(cantidad):
+    print("#"*cantidad)
+    
